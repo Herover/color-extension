9ffd4acfc2e0d65a6676699ff939a3940d4b1fe2
Herover <hi+git@leonora.app>
2022-09-22 15:03:47 +0200

    Correct offset to match red in top

--- color-popup/src/lib/HSLWheel.svelte
+++ color-popup/src/lib/HSLWheel.svelte
@@ -1,5 +1,6 @@
 <script>
 	import { onMount } from 'svelte';
+	import chroma from "chroma-js";
 
 	export let height = 400;
 	export let width = 400;
@@ -7,11 +8,15 @@
 	const radius = width/2;
 	const fullRadi = Math.PI * 2;
 
-	const angleSteps = 12
+	const angleSteps = 12;
 	const radiusSteps = 6;
 
 	const partRadius = 1/radiusSteps;
 	const partAngle = fullRadi / angleSteps;
+
+	const angleOffset = -Math.PI/2 - partAngle/2;
+
+
 
 	let canvas;
 	onMount(() => {
@@ -20,49 +25,49 @@
 
 		for (let angleStep = 0; angleStep < angleSteps; angleStep++) {
 			for (let radiusStep = 0; radiusStep < radiusSteps; radiusStep++) {
-				console.log(angleStep * (360/angleSteps))
-				ctx.fillStyle = `hsl(${angleStep * (360/angleSteps)}, ${radiusStep * (1/radiusSteps) * 100}%, 50%)`;
-				ctx.strokeStyle = `hsl(${angleStep * (360/angleSteps)}, ${radiusStep * (1/radiusSteps) * 100}%, 50%)`;
+				// @ts-ignore
+				const hsvColor = chroma.hsv(
+					angleStep * (360 / angleSteps),
+					// Add 1 to get 100% saturation but not 0%
+					(radiusStep + 1) * (1 / (radiusSteps)),
+					1,
+				);
+				const hslColor = hsvColor.css("hsl");
+
+				ctx.fillStyle = hslColor;
+				ctx.strokeStyle = hsvColor;
+
 				ctx.beginPath();
-				//ctx.moveTo(radius + Math.cos(partAngle) * radius*1, radius + Math.sin(partAngle) * radius*1);
+
 				ctx.arc(
 					width/2,
 					height/2,
 					radius * (partRadius * (radiusStep)),
-					partAngle * angleStep,
-					partAngle * (angleStep + 1),
+					angleOffset + partAngle * angleStep,
+					angleOffset + partAngle * (angleStep + 1),
 					false,
 				);
 				ctx.lineTo(
-					radius + Math.cos(partAngle * (angleStep + 1)) * radius * (partRadius * (radiusStep + 1)),
-					radius + Math.sin(partAngle * (angleStep + 1)) * radius * (partRadius * (radiusStep + 1)),
+					radius + Math.cos(angleOffset + partAngle * (angleStep + 1)) * radius * (partRadius * (radiusStep + 1)),
+					radius + Math.sin(angleOffset + partAngle * (angleStep + 1)) * radius * (partRadius * (radiusStep + 1)),
 				);
 				ctx.arc(
 					width/2,
 					height/2,
 					radius * (partRadius * (radiusStep + 1)),
-					partAngle * (angleStep + 1),
-					partAngle * angleStep,
+					angleOffset + partAngle * (angleStep + 1),
+					angleOffset + partAngle * angleStep,
 					true,
 				);
 				ctx.lineTo(
-					radius + Math.cos(partAngle * angleStep) * radius * (radiusSteps/(radiusStep)),
-					radius + Math.sin(partAngle * angleStep) * radius * (radiusSteps/(radiusStep)),
+					radius + Math.cos(angleOffset + partAngle * angleStep) * radius * (partRadius * (radiusStep)),
+					radius + Math.sin(angleOffset + partAngle * angleStep) * radius * (partRadius * (radiusStep)),
 				);
+				
 				ctx.fill();
 				ctx.stroke();
 			}
 		}
-		
-
-
-		/* ctx.beginPath();
-		ctx.arc(width/2, height/2, radius, partAngle * 1, partAngle * 2, false);
-		ctx.lineTo(radius + Math.cos(partAngle * 2) * radius*0.9, radius + Math.sin(partAngle * 2) * radius*0.9);
-		ctx.arc(width/2, height/2, radius * 0.9, partAngle * 2, partAngle * 1, true);
-		ctx.lineTo(radius + Math.cos(partAngle * 1) * radius*1, radius + Math.sin(partAngle * 1) * radius*1);
-		ctx.fill();
-		ctx.stroke(); */
 	});
 	
 </script>
