bb9331243b5f855bb8c48cdb2bc4855189597b88
Herover <hi+git@leonora.app>
2022-10-03 22:44:13 +0200

    Show swatch color lookalikes
    Lookalike is defined two colors with a CIE deltaE 2000 less than 1

--- color-popup/src/lib/SwatchList.svelte
+++ color-popup/src/lib/SwatchList.svelte
@@ -3,32 +3,70 @@
 
 
   export let swatch = [];
-  $: sortedSwatch = swatch.sort((a, b) => {
-    const aHSL = chroma(a.color).hsl();
-    const bHSL = chroma(b.color).hsl();
+  $: sortedSwatch = swatch
+    .sort((a, b) => {
+      const aHSL = chroma(a.color).hsl();
+      const bHSL = chroma(b.color).hsl();
 
-    const hueDiff = (bHSL[0] || -1) - (aHSL[0] || -1);
-    if (hueDiff == 0) {
-      const saturationDiff = bHSL[1] - aHSL[1];
-      if (saturationDiff == 0) {
-        const lightnessDiff = bHSL[2] - aHSL[2];
-        if (lightnessDiff == 0) {
-          // @ts-ignore because color.hsl() now returns 4 components
-          const alphaDiff = (bHSL[3] || 1) - (aHSL[3] || 1);
-          return alphaDiff;
+      const hueDiff = (bHSL[0] || -1) - (aHSL[0] || -1);
+      if (hueDiff == 0) {
+        const saturationDiff = bHSL[1] - aHSL[1];
+        if (saturationDiff == 0) {
+          const lightnessDiff = bHSL[2] - aHSL[2];
+          if (lightnessDiff == 0) {
+            // @ts-ignore because color.hsl() now returns 4 components
+            const alphaDiff = (bHSL[3] || 1) - (aHSL[3] || 1);
+            return alphaDiff;
+          }
+          return lightnessDiff;
         }
-        return lightnessDiff;
+        return saturationDiff;
       }
-      return saturationDiff;
-    }
-    return hueDiff;
-  });
+      return hueDiff;
+    })
+    .map(item => {
+      item.lookalikes = [];
+      return item;
+    })
+    .map((item, i) => {
+      const color1 = chroma(item.color);
+        if (color1.alpha() !== 1) {
+          return item;
+        }
+
+      // TODO: Could be faster by looping from i-swatch.length instead of this which is about n^2
+      item.lookalikes = swatch
+        .filter((item2, i2) => {
+          if (i == i2) {
+            return false;
+          }
+
+          const color2 = chroma(item2.color);
+
+          if (color2.alpha() !== 1) {
+            return;
+          }
+
+          // TODO: Define a constant to use
+          if (chroma.deltaE(color1, color2) < 1) {
+            return true;
+          }
+        })
+        .map(e => ({ color: e.color }));
+
+      return item;
+    });
 </script>
 
-{#each swatch as swatchItem}
+{#each sortedSwatch as swatchItem}
   <p>
     <span class="swatch-color" style="background-color:{swatchItem.color}"></span>
     {swatchItem.hsl}
+    {#if swatchItem.lookalikes.length}
+      {#each swatchItem.lookalikes as lookalike}
+        <span class="swatch-color" style="background-color:{lookalike.color}"></span>
+      {/each}
+    {/if}
   </p>
 {/each}
 
