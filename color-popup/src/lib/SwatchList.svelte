54fad3aedab3e4a4fe2f497008fc2b048ec3fcf0
Herover <hi+git@leonora.app>
2022-10-12 22:36:00 +0200

    Fix swatchlist sort issue

--- color-popup/src/lib/SwatchList.svelte
+++ color-popup/src/lib/SwatchList.svelte
@@ -4,27 +4,6 @@
 
   export let swatch = [];
   $: sortedSwatch = swatch
-    .map(e => e) // Shallow-copy to avoid mutating original value
-    .sort((a, b) => {
-      const aHSL = chroma(a.color).hsl();
-      const bHSL = chroma(b.color).hsl();
-
-      const hueDiff = (bHSL[0] || -1) - (aHSL[0] || -1);
-      if (hueDiff == 0) {
-        const saturationDiff = bHSL[1] - aHSL[1];
-        if (saturationDiff == 0) {
-          const lightnessDiff = bHSL[2] - aHSL[2];
-          if (lightnessDiff == 0) {
-            // @ts-ignore because color.hsl() now returns 4 components
-            const alphaDiff = (bHSL[3] || 1) - (aHSL[3] || 1);
-            return alphaDiff;
-          }
-          return lightnessDiff;
-        }
-        return saturationDiff;
-      }
-      return hueDiff;
-    })
     .map(item => {
       item.lookalikes = [];
       return item;
@@ -57,6 +36,27 @@
         .map(e => ({ color: e.color }));
 
       return item;
+    })
+    .map(e => e) // Shallow-copy to avoid mutating original array
+    .sort((a, b) => {
+      const aHSL = chroma(a.color).hsl();
+      const bHSL = chroma(b.color).hsl();
+
+      const hueDiff = (bHSL[0] || -1) - (aHSL[0] || -1);
+      if (hueDiff == 0) {
+        const saturationDiff = bHSL[1] - aHSL[1];
+        if (saturationDiff == 0) {
+          const lightnessDiff = bHSL[2] - aHSL[2];
+          if (lightnessDiff == 0) {
+            // @ts-ignore because color.hsl() now returns 4 components
+            const alphaDiff = (bHSL[3] || 1) - (aHSL[3] || 1);
+            return alphaDiff;
+          }
+          return lightnessDiff;
+        }
+        return saturationDiff;
+      }
+      return hueDiff;
     });
 </script>
 
