dd7ea8cce97f0999844c3af6e83a1dbd7f834b0b
Herover <hi+git@leonora.app>
2022-11-17 10:58:32 +0100

    Only use 2 decimals for lightness in hsl string

--- color-popup/src/lib/util.ts
+++ color-popup/src/lib/util.ts
@@ -1,5 +1,5 @@
 export const getHSLAString = (hue: number, saturation: number, lightness: number, alpha: number): string => {
-  const hslString = `${Math.floor(hue*100)/100}, ${Math.floor(saturation*10000)/100}%, ${lightness*100}%`;
+  const hslString = `${Math.floor(hue*100)/100}, ${Math.floor(saturation*10000)/100}%, ${Math.floor(lightness*10000)/100}%`;
   if (alpha !== 1) {
     return `hsla(${hslString},${alpha})`;
   } else {
