ee84049456d6bb2ff49fea00ec93a0d4ae32672c
Herover <hi+git@leonora.app>
2023-01-02 16:49:26 +0100

    Fix bug where some colors caused unresponsive ui

--- color-popup/src/lib/util.ts
+++ color-popup/src/lib/util.ts
@@ -6,7 +6,15 @@ export const clamp = (min: number, max: number, v: number): number => {
 
 export const getHSLAString = (hue: number, saturation: number, value: number, alpha: number): string => {
   const [nHue, nSaturation, nLightness] = chroma.hsv(hue, saturation, value).hsl();
-  const hslString = `${Math.floor(clamp(0, 360, nHue)*100)/100}, ${Math.floor(clamp(0, 1, nSaturation)*10000)/100}%, ${Math.floor(clamp(0, 1, nLightness)*10000)/100}%`;
+  // Since Chroma-js represents some components as NaN in certain situations (ex. hue when value is
+  // 0), we have to convert NaN values to something that doesn't break parsing the color.
+  const hslString =
+    Math.floor(clamp(0, 360, nHue || 0)*100)/100
+    + ", "
+    + Math.floor(clamp(0, 1, nSaturation || 0)*10000)/100
+    + "%, " 
+    + Math.floor(clamp(0, 1, nLightness || 0)*10000)/100
+    + "%";
   if (alpha !== 1) {
     return `hsla(${hslString},${clamp(0, 1, alpha)})`;
   } else {
